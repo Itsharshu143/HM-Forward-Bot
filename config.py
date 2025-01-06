6fc3cd59dfda0b1cc5463e4ddad0d2f13ba12db9
Itsharshu143 <116162890+Itsharshu143@users.noreply.github.com>
2025-01-06 16:34:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,13 +5,13 @@
 from os import environ 
 
 class Config:
-    API_ID = int(environ.get("API_ID", ""))
-    API_HASH = environ.get("API_HASH", "")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "") 
+    API_ID = int(environ.get("API_ID", "28642872"))
+    API_HASH = environ.get("API_HASH", "71590e471774c9dbbeab5a4c0e91ca07")
+    BOT_TOKEN = environ.get("BOT_TOKEN", "7884047572:AAFiqZn3QyZs9Bo0jfZOHDtWXbwXYW2pg40") 
     BOT_SESSION = environ.get("BOT_SESSION", "vjbot") 
-    DATABASE_URI = environ.get("DATABASE_URI", "")
-    DATABASE_NAME = environ.get("DATABASE_NAME", "vj-forward-bot")
-    BOT_OWNER = int(environ.get("BOT_OWNER", ""))
+    DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://Karthik:karthik1@cluster0.zwwg6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    DATABASE_NAME = environ.get("DATABASE_NAME", "Karthik")
+    BOT_OWNER = int(environ.get("BOT_OWNER", "6795403349"))
 
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
